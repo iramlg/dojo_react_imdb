e38e9292bdb7b3f7680af09948f46f86e907e201
Iram Garcia <iramlgarcia@gmail.com>
2017-07-14 18:36:35 -0300

    updating

--- srcc/assets/app.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { ContainerLogic } from './components/sample_survey.jsx';
-
-render(
-    <ContainerLogic name='Sampinha' />
-	, document.getElementById('reactDiv')
-)
